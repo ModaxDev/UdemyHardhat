191197549388a6a661f4d8cf0841c94252672fe1
Modax59 <61698516+ModaxDev@users.noreply.github.com>
2022-10-28 00:06:37 +0200

    feat: add thumbnail

--- contracts/Udemy.sol
+++ contracts/Udemy.sol
@@ -6,12 +6,20 @@ contract Udemy {
     uint public coursesCount = 0;
     mapping(uint => Course) public courses;
 
+
     struct Course {
         uint id;
         string title;
         string body;
         string thumbnail;
         address payable author;
+        Video[] videos;
+    }
+
+    struct Video {
+        uint id;
+        string title;
+        string hash;
     }
 
     event CourseCreated(
@@ -22,24 +30,36 @@ contract Udemy {
         address payable author
     );
 
-    function createCourse(string memory _title, string memory _body, string memory _thumbnail ) public {
+    function createCourse(string memory _title, string memory _body, string memory _thumbnail, string[] memory _videos, string[] memory _videosTitle) public {
         // Require valid title
         require(bytes(_title).length > 0);
         // Require valid body
         require(bytes(_body).length > 0);
         // Require valid thumbnail
         require(bytes(_thumbnail).length > 0);
+        // Require valid videos
+        require(_videos.length > 0);
         // Require valid author
         require(msg.sender != address(0x0));
         // Increment course count
+        uint256 videosAmount = _videos.length;
         coursesCount ++;
         // Create the course
-        courses[coursesCount] = Course(coursesCount, _title, _body, _thumbnail, payable(msg.sender));
+        for (uint i = 0; i < videosAmount; i ++) {
+            courses[coursesCount].videos.push(Video(
+                    i + 1,
+                    _videosTitle[i],
+                    _videos[i]
+                ));
+        }
+        courses[coursesCount].title = _title;
+        courses[coursesCount].body = _body;
+        courses[coursesCount].thumbnail = _thumbnail;
+        courses[coursesCount].author = payable(msg.sender);
+        courses[coursesCount].id = coursesCount;
         // Trigger an event
-        emit CourseCreated(coursesCount, _title, _body,_thumbnail, payable(msg.sender));
+        emit CourseCreated(coursesCount, _title, _body, _thumbnail, payable(msg.sender));
     }
-
-
 
 
     function getAllCourses() public view returns (Course[] memory) {
